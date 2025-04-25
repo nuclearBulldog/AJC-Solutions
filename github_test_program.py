d39c13673bc642bc2e321ddb079f5123c2a34323
Caleb Dellow <dellowcaleb@outlook.com>
2025-04-25 21:51:56 +1200

    Add files via upload

---
+++ github_test_program.py
@@ -0,0 +1,26 @@
+# GitHub Test program
+# Caleb Dellow, cad0489@arastudent.ac.nz
+# 25/04/2025
+
+print('Hello, User!')
+
+print()
+
+print('Hello, my name is Caleb!')
+
+print()
+
+print('Your turn!')
+
+# Copy the code from the README, in the repo with your name added here.
+
+print()
+
+print('Your name here')
+
+print()
+
+print('Your name here')
+
+print()
+
